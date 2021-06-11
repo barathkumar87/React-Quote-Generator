e5e2b753457e76400ad7a3bfaa17664a56f4fee6
Barath Kumar P S <73652035+barathkumar87@users.noreply.github.com>
2021-06-11 12:03:09 +0530

    Updates

---
+++ precache-manifest.7a3a0e1c7403bed394c4881aa3a66e3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99dbca32355f1e9eb3acb4616ae993f1",
+    "url": "/React-Quote-Generator/index.html"
+  },
+  {
+    "revision": "0521936bfb53b9c43eaa",
+    "url": "/React-Quote-Generator/static/css/main.6f97bbcd.chunk.css"
+  },
+  {
+    "revision": "b750add729089c4393d7",
+    "url": "/React-Quote-Generator/static/js/2.57509e35.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/React-Quote-Generator/static/js/2.57509e35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0521936bfb53b9c43eaa",
+    "url": "/React-Quote-Generator/static/js/main.a0c80cb7.chunk.js"
+  },
+  {
+    "revision": "3e73b2c14e6c083c75eb",
+    "url": "/React-Quote-Generator/static/js/runtime-main.33661fdc.js"
+  },
+  {
+    "revision": "f6dd574dfe99871b9f3bfe3eb1e38ea0",
+    "url": "/React-Quote-Generator/static/media/tumblr.f6dd574d.svg"
+  },
+  {
+    "revision": "642901af20b711c63556496841e4a8d5",
+    "url": "/React-Quote-Generator/static/media/twitter.642901af.svg"
+  }
+]);
